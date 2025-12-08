175a32994c4b58bf64c98b82f7e4785e07221608
LiZhenCheng9527 <lizhencheng6@huawei.com>
2025-12-08 17:29:47 +0800

    add copyright check in github workflow
    Signed-off-by: LiZhenCheng9527 <lizhencheng6@huawei.com>

--- sdk-python/agentcube/clients/constants.py
+++ sdk-python/agentcube/clients/constants.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 API_URL_ENV = "API_URL"
 API_TOKEN_ENV = "API_TOKEN"
 API_TOKEN_PATH = "/var/run/secrets/kubernetes.io/serviceaccount/token"
